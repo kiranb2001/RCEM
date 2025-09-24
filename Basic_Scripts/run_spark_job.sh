d1e0c5f97cc3d16fc47922f968ad6061926f09f3
Kiran.Beesanakoppa <kiranpb285@gmail.com>
2025-09-24 19:52:08 +0530

    please commit this

--- Basic_Scripts/run_spark_job.sh
+++ Basic_Scripts/run_spark_job.sh
@@ -15,7 +15,7 @@ PYSPARK_SCRIPT="/path/to/your_script.py"
 # Spark submit command
 spark-submit \
   --master yarn \
-  --deploy-mode client \
+  --deploy-mode cluster \
   --driver-memory ${DRIVER_MEMORY} \
   --executor-memory ${EXECUTOR_MEMORY} \
   --executor-cores ${EXECUTOR_CORES} \
